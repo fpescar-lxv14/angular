8465ccb9264117e98a40eddedfad1b2916113201
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-17 15:36:42 -0300

    establecer logica de formulario de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { HeaderComponent } from './components/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { JsonPipe } from '@angular/common';
+import { SessionService } from './services/session.service';
+import { Session } from './models/session.interface';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    JsonPipe,
     NavComponent,
     HeaderComponent,
     FooterComponent,
+  ],
+  providers: [
+    SessionService,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -18,4 +25,9 @@ import { NavComponent } from './components/nav/nav.component';
 export class AppComponent {
   title = 'Fundacion Pescar';
   subtitle = 'Capacitacion Angular'
+  session:Session = { username: "" };
+
+  constructor(@Inject() SessionService:Session){
+    this.session = SessionService
+  }
 }
--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
+    FormsModule,
     RouterLinkWithHref,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  @Output() submitLogin = new EventEmitter()
+  login = {
+    username:"",
+    password: ""
+  }
+  handleLogin (){
+    this.submitLogin.emit(this.login)
+  }
 }
