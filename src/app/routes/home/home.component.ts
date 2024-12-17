9dddb48d19751c02b38ee59f319c894669a31c44
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-17 16:51:31 -0300

    agregar mensaje condicional pagina principal

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { SessionService } from '../../services/session.service';
+import { Session } from '../../models/session.interface';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLinkWithHref,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private sessionService:SessionService){}
+  isLogged () {
+    return this.sessionService.getSession().username;
+  }
 }
