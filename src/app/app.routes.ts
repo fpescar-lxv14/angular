0a00905f60270fdb0c68a324b564348238888ca6
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-18 17:13:29 -0300

    instalar material y configurar rutas de aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./routes/home/home.component";
+import { ProfileComponent } from "./routes/profile/profile.component";
+import { SelectionComponent } from "./routes/selection/selection.component";
+import { CandidatesComponent } from "./routes/candidates/candidates.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent, 
+        pathMatch: 'full',
+    },
+    {
+        path: 'candidates', 
+        component: CandidatesComponent, 
+    },
+    {
+        path: 'selection', 
+        component: SelectionComponent, 
+    },
+    {
+        path: ':collection/:id', 
+        component: ProfileComponent, 
+    },
+    { 
+        path: '**', 
+        redirectTo: '/'
+    }
+];
