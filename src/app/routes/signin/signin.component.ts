26e6308de1fe43f12294962023fd234871c67856
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-18 13:07:09 -0300

    definir formulario de registro

--- src/app/routes/signin/signin.component.ts
+++ src/app/routes/signin/signin.component.ts
@@ -1,18 +1,39 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { SessionService } from '../../services/session.service';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
+  standalone: true,
   imports: [
+    ReactiveFormsModule,
     RouterLinkWithHref,
-  ],
-  providers: [
-    SessionService,
+    JsonPipe,
   ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
-  session = Inject(SessionService)
+  signin = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/[^@\W]+@\w+(\.\w+)*/)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^(?:(?=.*[A-Z])+(?=.*[a-z])+(?=.*[\d])+(?=.*[\W])).{8,}$/)
+    ])
+  })
+  constructor(private sessionService:SessionService){}
+  handleSubmmit(){
+  }
 }
