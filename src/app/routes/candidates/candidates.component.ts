11efa4a9b65da3bbfdebec3776ccb1e48847bb23
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-20 12:24:48 -0300

    agregar estilos y refactorizar seleccion de candidatos

--- src/app/routes/candidates/candidates.component.ts
+++ src/app/routes/candidates/candidates.component.ts
@@ -2,38 +2,40 @@ import { Component } from '@angular/core';
 import { CandidatesService } from '../../services/candidates.service';
 import { JsonPipe } from '@angular/common';
 import { InfocardComponent } from '../../components/infocard/infocard.component';
+import { Candidate } from '../../models/candidate.interface';
+import { MatButton } from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-candidates',
   imports: [
+    MatGridListModule,
+    MatButton,
     JsonPipe,
-    InfocardComponent
+    InfocardComponent,
   ],
   templateUrl: './candidates.component.html',
   styleUrl: './candidates.component.css'
 })
 export class CandidatesComponent {
-  users:any=[]
+  users:Candidate[]=[]
   error:any = null
   constructor(private candidateService:CandidatesService){
     this.candidateService.getCandidates('?results=24')
     .subscribe({
-      next: (res:any) => { this.users = res?.results },
+      next: (res:any) => { this.users = res.results },
       error: (error) => { this.error = error}
   })}
   getData(n="?results=12"){
     this.candidateService.getCandidates(n)
       .subscribe({
-        next: (res:any) => { this.users.push(res?.results) },
+        next: (res:any) => { this.users.push(...res?.results) },
         error: (error) => { this.error = error }
       })
   }
   replace(id:string){
-    // Agregandolo al final del array
-    this.users = this.users.filter((item:any) => id !== item.login.uuid)
-    this.getData('')
-    // // Reemplazando posicion
-    // const data = this.candidateService.getCandidates(v => v.results)
-    // this.users = this.users.map(item => item.login.uuid == id ? data : item)
+    this.users = this.users.filter((item:any) => id !== item?.login?.uuid);
+    this.getData('');
   }
 }
