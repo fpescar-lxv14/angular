51df60c082067941cb4fb40852b0b8f5417310fd
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-20 12:25:27 -0300

    establecer servicio de seleccion

---
+++ src/app/services/selection.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Candidate } from '../models/candidate.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SelectionService {
+  url = "https://expressserver.onrender.com/selection/process";
+  selection:Candidate[] = [];
+  constructor(private http:HttpClient){}
+  getCandidates(){
+    return this.selection;
+  }
+  selectCandidate(candidate:Candidate){
+    this.selection.push(candidate)
+  }
+  removeCandidate(id:string){
+    this.selection = this.selection.filter((s:any) => s.login.uuid !== id)
+  }
+  uploadSelection(data:any){
+    this.http.post(this.url, data)
+  }
+}
